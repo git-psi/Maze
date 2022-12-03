e3b889fb9541e84f858b103a5858b7795b31c4a3
git-psi <perdu.felix@free.fr>
2022-12-03 18:54:29 +0100

    new class

--- main.py
+++ main.py
@@ -1,79 +1,22 @@
 import pygame
-import random
+import maze_creator as maze_creator_py
 pygame.init()
 
 clock = pygame.time.Clock()
-fps = 15
+fps = 60
 
 #case of the screen
 num_of_case = 41
-case_size = 10
+case_size = 20
 
 #initialize screen
 screen_width =  num_of_case * case_size
 screen_height = num_of_case * case_size
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-def start():
-    #initialize variable for the creation
-    move_stack = []
-    maze_data = []
-    for i in range(0, num_of_case):
-        line = []
-        for i in range(0, num_of_case):
-            line.append(0)
-        maze_data.append(line)
-    maze_data[1][1] = 1
-    maze_data[1][0] = 1
-    maze_data[num_of_case - 2][num_of_case - 1] = 1
-
-    #initialize position
-    x = 1
-    y = 1
-    return x, y, maze_data, move_stack
-x, y, maze_data, move_stack = start()
-
-def move(x, y, maze_data, move_stack):
-    possibility = []
-    #see possibility of deplacement
-    if x-2 > 0 and maze_data[y][x-2] == 0:
-        possibility.append("west")
-    if x+2 < num_of_case - 1:
-        if maze_data[y][x+2] == 0:
-            possibility.append("east")
-    if y-2 > 0 and maze_data[y-2][x] == 0:
-        possibility.append("north")
-    if y+2 < num_of_case - 1:
-        if maze_data[y+2][x] == 0:
-            possibility.append("south")
-
-    if possibility:
-        move = random.choice(possibility)
-        if move == "west":
-            maze_data[y][x-1] = 1
-            maze_data[y][x-2] = 1
-            x -= 2
-        if move == "east":
-            maze_data[y][x+1] = 1
-            maze_data[y][x+2] = 1
-            x += 2
-        if move == "north":
-            maze_data[y-1][x] = 1
-            maze_data[y-2][x] = 1
-            y -= 2
-        if move == "south":
-            maze_data[y+1][x] = 1
-            maze_data[y+2][x] = 1
-            y += 2
-        move_stack.append((x, y))
-    else:
-        if move_stack:
-            x, y = move_stack.pop()
-        else:
-            if pygame.key.get_pressed()[pygame.K_RETURN]:
-                x, y, maze_data, move_stack = start()
-
-    return x, y, maze_data, move_stack
+#create maze
+maze_creator = maze_creator_py.MazeCreator()
+maze_data = maze_creator.create_maze(num_of_case, case_size)
 
 def draw_maze(maze_data, case_size):
     line = 0
@@ -92,12 +35,8 @@ while run:
 
     #draw background
     pygame.draw.rect(screen, (0, 0, 0), (0, 0, screen_width, screen_height))
-    #move
-    x, y, maze_data, move_stack = move(x, y, maze_data, move_stack)
     #draw maze
     draw_maze(maze_data, case_size)
-    #draw x and y
-    # pygame.draw.rect(screen, (255, 0, 0), (x * case_size, y * case_size, case_size, case_size))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
---
+++ maze_creator.py
@@ -0,0 +1,71 @@
+import random
+import copy
+import pygame
+
+class MazeCreator():
+    def __init__(self):
+        pass
+
+    def create_empty_maze(self):
+        #initialize variable for the creation
+        self.move_stack = []
+        self.maze_data = []
+        for i in range(0, self.num_of_case):
+            line = []
+            for i in range(0, self.num_of_case):
+                line.append(0)
+            self.maze_data.append(line)
+        self.maze_data[1][1] = 1
+        self.maze_data[1][0] = 1
+        self.maze_data[self.num_of_case - 2][self.num_of_case - 1] = 1
+        #initialize position
+        self.x = 1
+        self.y = 1
+
+    def create_maze(self, num_of_case, case_size):
+        #case of the screen
+        self.num_of_case = num_of_case
+        self.case_size = case_size
+
+        self.create_empty_maze()
+        load_maze = True
+
+        while load_maze:
+            possibility = []
+            #see possibility of deplacement
+            if self.x-2 > 0 and self.maze_data[self.y][self.x-2] == 0:
+                possibility.append("west")
+            if self.x+2 < num_of_case - 1:
+                if self.maze_data[self.y][self.x+2] == 0:
+                    possibility.append("east")
+            if self.y-2 > 0 and self.maze_data[self.y-2][self.x] == 0:
+                possibility.append("north")
+            if self.y+2 < num_of_case - 1:
+                if self.maze_data[self.y+2][self.x] == 0:
+                    possibility.append("south")
+
+            if possibility:
+                move = random.choice(possibility)
+                if move == "west":
+                    self.maze_data[self.y][self.x-1] = 1
+                    self.maze_data[self.y][self.x-2] = 1
+                    self.x -= 2
+                if move == "east":
+                    self.maze_data[self.y][self.x+1] = 1
+                    self.maze_data[self.y][self.x+2] = 1
+                    self.x += 2
+                if move == "north":
+                    self.maze_data[self.y-1][self.x] = 1
+                    self.maze_data[self.y-2][self.x] = 1
+                    self.y -= 2
+                if move == "south":
+                    self.maze_data[self.y+1][self.x] = 1
+                    self.maze_data[self.y+2][self.x] = 1
+                    self.y += 2
+                self.move_stack.append((copy.deepcopy(self.x), copy.deepcopy(self.y)))
+            else:
+                if self.move_stack:
+                    self.x, self.y = self.move_stack.pop()
+                else:
+                    load_maze = False
+        return self.maze_data
